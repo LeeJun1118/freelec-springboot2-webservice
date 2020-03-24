4f5937c301df4a0a74e7a5abdcc668cde49354ca
ljun1118 <ejun1118@gmail.com>
2020-03-24 17:32:27 +0900

    Modify : 에러 잡는 중

--- src/main/java/com/jun/lect/springboot/config/oauth/dto/OAuthAttributes.java
+++ src/main/java/com/jun/lect/springboot/config/oauth/dto/OAuthAttributes.java
@@ -36,23 +36,23 @@ public class OAuthAttributes {
 
 
 
-    private static OAuthAttributes ofGoogle(String userNameAttributeName,Map<String,Object> attributes){
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
-                .name((String)attributes.get("name"))
-                .email((String)attributes.get("email"))
-                .picture((String)attributes.get("picture"))
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
-        Map<String,Object> response = (Map<String,Object>)attributes.get("response");
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
-                .name((String)response.get("name"))
-                .email((String)response.get("email"))
-                .picture((String)response.get("profile_image"))
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
@@ -62,7 +62,7 @@ public class OAuthAttributes {
     //OAuthAttributes 에서 엔티티를 생성하는 시점은 처음 가입할 때
     //가입할 때의 기본 권한을 GUEST 로 주기 위해서 role 빌더 값에는 Role.GUEST 를 사용
     //OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성
-    public User toEntity(){
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)

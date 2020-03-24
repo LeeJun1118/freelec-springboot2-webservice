db2764ec357783d0becec6e9d27ecdce73a8dc35
ljun1118 <ejun1118@gmail.com>
2020-03-24 17:56:51 +0900

    Modify:클래스명

--- src/main/java/com/jun/lect/springboot/config/WebConfig.java
+++ src/main/java/com/jun/lect/springboot/config/WebConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class Webconfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
